b5f6a107430c90e912e16cfd9f4bd23d904b3555
larocquesimon <larocquesimon@gmail.com>
2015-04-07 00:43:56 -0400

    simpler version: EchoXML project with only the service, not all the pm1 stuff... and changed the url to the wsdl

--- PM1Official/src/java/soen487/retriever/services/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.retriever.soen487/")
-package soen487.retriever.services.client;
--- PM1Official/src/java/soen487/wscat/services/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.wscat.soen487/")
-package soen487.wscat.services.client;
